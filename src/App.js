c1d077cb625eea9872e91254c9f6af8703b90fb4
Diel Duarte <magdiel.kesller@gmail.com>
2020-09-02 17:06:15 -0300

    add inspector

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import { useMachine } from '@xstate/react';
-import styled from 'styled-components';
+import React from "react";
+import { useMachine } from "@xstate/react";
+import styled from "styled-components";
+import { inspect } from "@xstate/inspect";
 
-import gameMachine from './state/index';
-import IconsGrid from './components/IconsGrid';
-import GameDisplay from './components/GameDisplay';
-import UserPoints from './components/UserPoints';
-import useJoyCon from './useJoyConSocket';
-import useSocket from './useSocket';
+import gameMachine from "./state/index";
+import IconsGrid from "./components/IconsGrid";
+import GameDisplay from "./components/GameDisplay";
+import UserPoints from "./components/UserPoints";
+import useJoyCon from "./useJoyConSocket";
+import useSocket from "./useSocket";
+
+inspect({
+  url: "https://statecharts.io/inspect",
+  iframe: false,
+});
 
 const GameDisplayContainer = styled.div`
   display: flex;
